3f95de354babf9bdf893dcbdae91eceac5ff987b
wisdomsena <wisdomsena36@gmail.com>
2023-06-19 23:21:23 -0700

    Add code to print all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,10 +15,10 @@ int main(void)
 		for (number2 = number1 + 1; number2 <= 99; number2++)
 		{
 			putchar((number1 / 10) + '0');
-			putchar((number1 / 10) + '0');
+			putchar((number1 % 10) + '0');
 			putchar(' ');
 			putchar((number2 / 10) + '0');
-			putchar((number2 / 10) + '0');
+			putchar((number2 % 10) + '0');
 			if (!(number1 == 98 && number2 == 99))
 			{
 				putchar(',');
