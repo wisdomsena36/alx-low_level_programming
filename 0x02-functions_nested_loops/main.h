aeb51eaf8e3620480a0220800a3ba514ae8d4551
wisdomsena <wisdomsena36@gmail.com>
2023-06-21 03:16:05 -0700

    Add code to print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int);
+int _isalpha(int);
+int print_sign(int);
+int _abs(int);
+int print_last_digit(int);
+void jack_lower(void);
+void times_tablet(void);
+int add(int, int);
+void print_to_98(int);
+void print_times_tablet(int);
