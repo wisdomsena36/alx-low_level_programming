230b011363fbc3bb006e0f7213e0745e9f73ce0c
wisdomsena <wisdomsena36@gmail.com>
2023-06-21 22:59:29 -0700

    Add code that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
-                _putchar(',');
-                _putchar(' ');
                 return (-1);
 	}
 	else
 	{
-		_putchar('0');
-                _putchar(',');
-                _putchar(' ');
 		return (0);
 	}
 }
