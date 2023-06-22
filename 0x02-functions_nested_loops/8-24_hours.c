6ab38a0cc9ef4b3d004d5dc8f3ee53fe65f03824
wisdomsena <wisdomsena36@gmail.com>
2023-06-22 00:48:48 -0700

    Add code that prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			_putchar(hour + '0');
-			_putchar(hour + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar(minute + '0');
-			_putchar(minute + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
