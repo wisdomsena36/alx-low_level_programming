fc1d8357b79b098c9706a15a17d089eb932bcde6
wisdomsena <wisdomsena36@gmail.com>
2023-06-26 19:56:32 -0700

    Add code that copies the string pointed

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - It copies a string.
+ * @dest: A pointer to the destination string 
+ * where the copied string will be stored.
+ * @src: A pointer to the source string that will be copied.
+ *
+ * Return: A pointer to the destination string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	len = 0;
+	while (src[len] != '\0')
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
