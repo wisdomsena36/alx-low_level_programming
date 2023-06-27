2b58f496933c867b4420af22fab6f0775bfd651e
wisdomsena <wisdomsena36@gmail.com>
2023-06-26 21:21:54 -0700

    Add code that generates random valid passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - It generates randam valid passwords.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int password[100];
+	int i;
+	int total;
+	int num;
+
+	i = 0;
+	total = 0;
+	srand(time(NULL));
+	while (i < 100)
+	{
+		password[i] = rand() % 90;
+		total += (password[i] + '0');
+		putchar(password[i] + '0');
+		if ((2772 - total) - '0' < 78)
+		{
+			num = 2772 - total - '0';
+			total += num;
+			putchar(num + '0');
+			break;
+		}
+	}
+	return (0);
+}
