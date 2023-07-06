fa8887d107f548eaa5f57b5a8e5078b54800c319
wisdomsena <wisdomsena36@gmail.com>
2023-07-06 06:48:35 -0700

    Add code that compares two strings.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,13 +14,15 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 == '\0')
 			return (1);
-		if (*s21 == '*')
+		if (*s2 == '*')
 			return (wildcmp(s1, s2 + 1));
 	}
 
 	if (*s2 == '*')
+	{
 		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 			return (1);
+	}
 	else if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	return (0);
