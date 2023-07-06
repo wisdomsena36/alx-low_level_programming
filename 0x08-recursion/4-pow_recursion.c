22bfea306f60f9fa0743b7e486a9223ae94871ce
wisdomsena <wisdomsena36@gmail.com>
2023-07-05 19:13:11 -0700

    Add that returns the value of a number raised to the power of another number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - It print the value of a number raised to
+ * the power of another number.
+ * @x: The base number.
+ * @y: The exponent number.
+ *
+ * Return: The value of a number raised to another number.
+ * If exponent number is less than 0 return -1.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x* _pow_recursion(x, y - 1));
+}
