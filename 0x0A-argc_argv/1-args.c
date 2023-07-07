86aa8a92958eebf82754389dc34ea4b5f6b888dc
wisdomsena <wisdomsena36@gmail.com>
2023-07-07 15:20:39 -0700

    Add code that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry point.
+ * @argc: It count the number of array in argv.
+ * @argv: It is the vector.
+ *
+ * Return: Always 0 for Success.
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
