144a55a06b2b859807f1858b8e57f0ade4cd170b
wisdomsena <wisdomsena36@gmail.com>
2023-07-07 14:53:39 -0700

    Add code that prints its name.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry point.
+ * @argc: It counts the array.
+ * @argv: The vectors.
+ *
+ * Return: Always 0 for Success.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
