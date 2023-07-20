d0f4d5c0972bc97c7bccc8a326b345ec41b790fc
wisdomsena <wisdomsena36@gmail.com>
2023-07-20 15:21:20 -0700

    Add variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,27 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct format_t - It Typedef for the struct format_t.
+ * @format: The format character.
+ * @ptr: The function pointer to the printing function.
+ */
+typedef struct format_t
+{
+	char format;
+	void (*ptr)(va_list args);
+} format_t;
+
+void print_char(va_list c_args);
+void print_integer(va_list i_args);
+void print_float(va_list f_args);
+void print_string(va_list s_args);
 
 #endif /* VARIADIC_FUNCTIONS_H */
