1a75ffa0930037b421d085cb331e124ffab78fc0
wisdomsena <wisdomsena36@gmail.com>
2023-08-01 15:21:22 -0700

    Fix 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,42 +1,61 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint_safe - It prints a singly linked
- * list of integers, even if it has a loop.
+ * print_listint_safe - Prints a listint_t linked list (handles loops).
  * @head: A pointer to the head of the linked list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow;
-	const listint_t *fast;
-	size_t count;
+	const listint_t *slow, *fast;
+	size_t count = 0;
 
 	slow = head;
 	fast = head;
 
-	while (slow && fast && fast->next)
+	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
 		slow = slow->next;
 		fast = fast->next->next;
 
 		if (slow == fast)
 		{
-			printf("-> [%p] %d\n", (void *)fast, fast->n);
+			/* List contains a loop, break the loop to avoid infinite loop */
+			printf("[%p] %d\n", (void *)slow, slow->n);
 			break;
 		}
+		count++;
 	}
 
-	if (!slow || !fast || !fast->next)
+	if (slow != fast)
 	{
-		while (head)
+		/* List does not contain a loop, continue printing */
+		while (slow != NULL)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
 			count++;
 		}
+	}
+	else
+	{
+		/* List contains a loop, stop printing to avoid infinite loop */
+		listint_t *loop_start = (listint_t *)slow;
+		do
+		{
+			printf("[%p] %d\n", (void *)slow, slow->n);
+			slow = slow->next;
+			count++;
+		} while (slow != loop_start);
+		printf("-> [%p] %d\n", (void *)loop_start, loop_start->n);
+
+		/* Exit the program with status 98 on failure */
+		exit(98);
 	}
 
 	return (count);
