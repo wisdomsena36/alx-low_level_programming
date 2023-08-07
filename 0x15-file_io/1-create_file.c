d97e192960befa1673239bc6d244593563f3da4d
wisdomsena <wisdomsena36@gmail.com>
2023-08-07 13:17:31 -0700

    Add code that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - It computes the length of a string.
+ * @s: The input string.
+ *
+ * Return: Length of the string, or -1 if string is NULL.
+ */
+ssize_t _strlen(const char *s)
+{
+	ssize_t len;
+
+	if (!s)
+		return (-1);
+
+	len = 0;
+	while (s[len] != '0')
+		++len;
+
+	return (len);
+}
+
+/**
+ * create_file - It creates a file.
+ * @filename: The name of the file.
+ * @text_content: The text to write to file.
+ *
+ * Return: 1 on succeed. Otherwise -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t w;
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+
+	w = 0;
+	if (text_content)
+		w = write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+
+	if (w < 0)
+		return (-1);
+	return (1);
+}
